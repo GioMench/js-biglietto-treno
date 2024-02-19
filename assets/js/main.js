908163291ddbae3cf97f595a9ba0b363e009495b
GioMench <giorgiaglm@gmail.com>
2024-02-19 16:56:55 +0100

    change final price

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,23 @@ console.log(parseInt(agePassenger));
 //se età minore di 18 prezzo = 0.21 * km -20%
 //se età maggiore di 65 prezzo = 0.21 * km -40%    
 //il prezzo finale deve essere in forma umana 
-if(agePassenger < 18){
+
+/*if(agePassenger < 18){
     console.log((Math.round(distance * 0.21 - ((distance * 0.21)* 20 / 100))*100)/100);  
 } else if(agePassenger > 65){
     console.log((Math.round(distance * 0.21 - ((distance * 0.21)* 40 / 100))*100)/100); 
 }else{
     console.log((Math.round(distance * 0.21)*100)/100);
-}
+}*/
 
+
+if(agePassenger < 18){
+    console.log(((distance * 0.21 - ((distance * 0.21)* 20 / 100))).toFixed(2));  
+} else if(agePassenger > 65){
+    console.log(((distance * 0.21 - ((distance * 0.21)* 40 / 100))).toFixed(2)); 
+}else{
+    console.log((distance * 0.21).toFixed(2));
+}
 
 
     
